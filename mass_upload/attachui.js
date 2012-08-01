2c2784a404bab46bb93545cdd449be1cef50a410
Nao <nao@wedge>
2012-08-01 14:05:39 +0000

    + [mass_upload] Initial commit of mass_upload, equivalent of http://github.com/Naowe/Multi-Attach/ -- I hate you github, I really do.

---
+++ mass_upload/attachui.js
@@ -0,0 +1,268 @@
+/**
+ * Multiple attachment basic JavaScript file, contains the entire UI functions
+ *
+ * @package Dragooon:MultiAttach
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2012, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *		Licensed under "New BSD License (3-clause version)"
+ *		http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0
+ */
+
+// One method would've been to hook into Wedge's attach functions, but since there are quite a lot of fundamental differences
+// between the workings, I decided to write my own instead.
+$(function (jQuery, undefined)
+{
+	// No point in this if we cannot support XHR level 2 upload
+	if (!(window.ProgressEvent && window.FormData && window.XMLHttpRequest && 'withCredentials' in new XMLHttpRequest))
+		return true;
+
+	var
+		$element = $('#attachments_container > input[type=file]:first'),
+		$files = [],
+		$current = -1,
+		$is_uploading = false,
+		total_size = 0,
+		xhr = null;
+
+	$element
+		// Release this input of the default chains, we got new ones!
+		.unbind('change')
+		// Update this element to support multiple attachments
+		.attr('name', 'attachment_holder')
+		// Bind the "change" event to properly handle multiple attachments into upload
+		.change(function () { return attachFiles(this.files || {}, 0); });
+
+	$('<div id="dropnotice" style="text-align: center; border: 1px solid black; padding: 20px" class="windowbg2"><div class="largetext">' + txt_drag_help + '</div><div class="mediumtext">' + txt_drag_help_subtext  + '</div></div>')
+		.hide()
+		.prependTo($element.parent());
+
+	var dragUIOpened = false, dragTimer = +new Date();
+
+	$(document.body)
+		.bind('dragover', function (e)
+		{
+			e.originalEvent.dataTransfer.dropEffect = 'none';
+
+			// Expand the additional option if it's collapsed
+			if (!dragUIOpened)
+			{
+				if (!$('#postAttachment2').is(':visible'))
+					$('#postMoreExpandLink').data('that').toggle();
+
+				// Show a neat "Drop the file here" notice
+				$element.fadeOut('fast', function () { $('#dropnotice').fadeIn(); });
+				dragUIOpened = true;
+			}
+			dragTimer = +new Date();
+
+			return false;
+		})
+		.bind('dragleave', function ()
+		{
+			setTimeout(function ()
+			{
+				if ((+new Date()) - dragTimer > 200)
+				{
+					$('#dropnotice').fadeOut('fast', function () { $element.fadeIn(); });
+					dragUIOpened = false;
+				}
+			}, 200);
+		});
+
+	$('#dropnotice')
+		.bind('dragover', function (e)
+		{
+			dragTimer = +new Date();
+			e.originalEvent.dataTransfer.dropEffect = 'copy';
+
+			return false;
+		})
+		.bind('drop', function (e)
+		{
+			// Make sure we are dragging a file over
+			if (!e.originalEvent.dataTransfer && !(dt.files || (!$.browser.webkit && e.originalEvent.dataTransfer.types.contains && e.originalEvent.dataTransfer.types.contains('Files'))))
+				return false;
+
+			dragUIOpened = false;
+
+			var files = e.originalEvent.dataTransfer.files;
+			$('#dropnotice').fadeOut('fast', function ()
+			{
+				$element.fadeIn(function () { attachFiles(files, 0); });
+			});
+
+			return false;
+		});
+
+	var startUpload = function ()
+	{
+		if ($is_uploading)
+			return true;
+
+		// Are we done?
+		if ($files[++$current] === undefined)
+		{
+			$current--;
+			return true;
+		}
+
+		$is_uploading = true;
+		var
+			$timer = +new Date(),
+			$progress = $('<div class="windowbg2 inline-block middle" style="height: 16px; width: 150px; margin: 5px 10px; border: 1px solid #666"><div class="plainbox" style="background: #c2d3ca; height: 12px; padding: 0; border-radius: 0; border: 0; width: 0"></div></div>')
+				.appendTo($files[$current].element);
+
+		xhr = new XMLHttpRequest();
+		xhr.open('POST', weUrl('action=multiattach;board=' + we_board));
+		xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+		xhr.setRequestHeader('X-File-Name', $files[$current].fileName || $files[$current].name);
+		xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+		xhr.upload.onprogress = function (e)
+		{
+			if (e.lengthComputable && (+new Date()) - $timer > 500)
+			{
+				$timer = +new Date();
+				$progress.find('.plainbox').width((e.loaded / e.total) * 150);
+			}
+		};
+		xhr.onreadystatechange = function (e)
+		{
+			if (xhr.readyState == 4 && xhr.status == 200)
+			{
+				var $response = $.parseJSON(xhr.responseText);
+				$progress.remove();
+
+				// !! @todo: still needs to be able to handle removal requests...
+				if ($response.valid)
+				{
+					$files[$current].element
+						.find('.delete').val(we_delete).end()
+						.find('span').css('font-style', '');
+					$('input[name="attach_del\[\]"]').last().closest('dd').after('<dd class="smalltext"><label><input type="checkbox" id="attachment_' + $response.id + '" name="attach_del[]" value="' + $response.id + '" checked onclick="oAttach().checkActive();" /> ' + $response.name + '</label></dd>');
+				}
+				else
+					$files[$current].element
+						.find('.delete').remove().end()
+						.find('span').css('color', 'red')
+						.append('<br>' + $response.error);
+
+				// Move onto the next file
+				$is_uploading = false;
+				startUpload();
+			}
+		};
+
+		xhr.send($files[$current]);
+	},
+
+	attachFiles = function (files, i)
+	{
+		if (files[i] === undefined)
+			return true;
+
+		// Check for file's extension
+		var
+			filename = files[i].fileName || files[i].name,
+			filesize = files[i].fileSize || files[i].size,
+			extension = filename.substr(filename.lastIndexOf('.') + 1, filename.length).toLowerCase();
+
+		if (attachOpts.checkExtension && !in_array(extension, attachOpts.validExtensions))
+		{
+			alert(attachOpts.ext_error.replace('{ext}', extension));
+			return attachFiles(files, ++i);
+		}
+
+		// Check number of files
+		if (attachOpts.maxNum > 0 && attachOpts.currentNum + $files.length > attachOpts.maxNum)
+		{
+			alert(attachOpts.maxNum_error);
+			return;
+		}
+
+		// Check for file's size
+		if (attachOpts.sizeLimit > 0 && filesize / 1024 > attachOpts.sizeLimit)
+		{
+			alert(attachOpts.filesize_error);
+			return attachFiles(files, ++i);
+		}
+
+		// Check total file's size
+		if (attachOpts.totalSizeLimit > 0 && (filesize / 1024 + attachOpts.totalSize + total_size) > attachOpts.totalSizeLimit)
+		{
+			alert(attachOpts.totalFilesize_error);
+			return;
+		}
+
+		var $container = $('<div style="margin-top: 8px">');
+
+		$('<input type="button" class="delete" style="margin-top: 4px">')
+			.val(we_cancel)
+			.click(function ()
+			{
+				var i = $(this).parent().data('id'), n = i + 1, len = $files.length;
+
+				$(this).parent().remove();
+
+				delete $files[i];
+
+				// Shift consecutive file element's index
+				for (; n < len; n++)
+				{
+					var file = $files[n];
+					file.element.data('id', n - 1);
+					$files[n - 1] = file;
+					delete $files[n];
+				}
+
+				// This the one being uploaded?
+				if (i == $current && $is_uploading)
+				{
+					xhr.abort();
+					$is_uploading = false;
+					$current--;
+					startUpload();
+				}
+			})
+			.appendTo($container);
+
+		$('<span style="margin: 5px 10px; font-style: italic">')
+			.text(files[i].fileName || files[i].name)
+			.appendTo($container);
+
+		if (/^image\//.test(files[i].type))
+		{
+			var imgPreview = new FileReader();
+
+			if (imgPreview)
+			{
+				imgPreview.onload = function (e)
+				{
+					$('<img class="middle">')
+						.appendTo($container)
+						.load(function () {
+							$(this)
+								.height(Math.min($(this).height(), 50))
+								.show();
+						})
+						.attr('src', e.target.result)
+						.hide();
+				};
+				imgPreview.readAsDataURL(files[i]);
+			}
+		}
+
+		$container.appendTo($element.parent());
+		$files[$files.length] = files[i];
+		$files[$files.length - 1].element = $container;
+		$container.data('id', $files.length - 1);
+		total_size += filesize / 1024;
+
+		// Always start upload automatically, it'll automatically skip if in progress
+		startUpload();
+
+		return attachFiles(files, ++i);
+	};
+});
---
+++ mass_upload/plugin.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Multiple attachment plugin files, contains hook callback and pretty much everything
+ *
+ * @package Dragooon:MultiAttach
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2012, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *		Licensed under "New BSD License (3-clause version)"
+ *		http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 1.0
+ */
+
+/**
+ * Callback for hook post_form_pre, loads the javascript for manipulating the upload via AJAX
+ *
+ * @return void
+ */
+function multiattach_post_form_pre()
+{
+	global $context, $board, $topic, $txt, $settings;
+
+	if (!allowedTo('post_attachment'))
+		return;
+
+	loadPluginLanguage('Dragooon:MultiAttach', 'plugin');
+	loadLanguage('Errors');
+
+	$current_attach_dir = get_attach_dir();
+
+	$total_size = 0;
+	if (!isset($_SESSION['temp_attachments']))
+		$_SESSION['temp_attachments'] = array();
+	foreach ($_SESSION['temp_attachments'] as $attach => $filename)
+		$total_size += filesize($current_attach_dir . '/' . $attach);
+
+	add_plugin_js_file('Dragooon:MultiAttach', 'attachui.js');
+	add_js('
+	txt_drag_help = ', JavaScriptEscape($txt['multiattach_drag_help']), ';
+	txt_drag_help_subtext = ', JavaScriptEscape($txt['multiattach_drag_help_subtext']), ';
+	attachOpts = {
+		sizeLimit: ', $settings['attachmentSizeLimit'], ',
+		totalSizeLimit: ', $settings['attachmentPostLimit'], ',
+		maxNum: ', $settings['attachmentNumPerPostLimit'], ',
+		currentNum: ', count($_SESSION['temp_attachments']), ',
+		checkExtension: ', !empty($settings['attachmentCheckExtensions']) ? 'true' : 'false', ',
+		validExtensions: ', JavaScriptEscape($settings['attachmentExtensions']), '.split(","),
+		totalSize: ', round($total_size / 1024), ',
+		ext_error: ', JavaScriptEscape(str_replace('{attach_exts}', strtr($settings['attachmentExtensions'], array(',' => ', ')), $txt['cannot_attach_ext'])), ',
+		filesize_error: ', sprintf(JavaScriptEscape($txt['file_too_big']), $settings['attachmentSizeLimit']), ',
+		maxNum_error: ', sprintf(JavaScriptEscape($txt['attachments_limit_per_post']), $settings['attachmentNumPerPostLimit']), ',
+		totalFilesize_error: ', sprintf(JavaScriptEscape($txt['file_too_big']), $settings['attachmentPostLimit']), '
+	};');
+}
+
+/**
+ * Action handler for multiattach, handles uploading of files via AJAX
+ * A fair amount of this is borrowed from Post.php
+ *
+ * @return void
+ */
+function multiattach()
+{
+	global $settings, $topic, $scripturl, $board, $options, $language, $user_info, $board, $context;
+
+	header('Content-type: text/plain; charset=utf-8');
+
+	// No board?
+	if (empty($board) && empty($context['allow_no_board']))
+		multiattach_error('no_board');
+
+	// Not allowed to post attachments?
+	if (!allowedTo('post_attachment'))
+		multiattach_error('permission_denied');
+
+	$current_attach_dir = get_attach_dir();
+
+	$stream = fopen('php://input', 'r');
+	$filename = isset($_SERVER['HTTP_X_FILE_NAME']) ? $_SERVER['HTTP_X_FILE_NAME'] : '';
+
+	if (empty($filename) || !is_writable($current_attach_dir))
+		multiattach_error('invalid_filename');
+
+	// Check for extensions
+	if (!empty($settings['attachmentCheckExtensions']))
+		if (!in_array(strtolower(substr(strrchr($filename, '.'), 1)), explode(',', strtolower($settings['attachmentExtensions']))))
+			multiattach_error($txt['cant_upload_type'] . ' ' . $settings['attachmentExtensions']);
+
+	$attachID = 'post_tmp_' . $user_info['id'] . '_' . (count($_SESSION['temp_attachments']) + 1);
+	$dest = $current_attach_dir . '/' . $attachID;
+
+	$target = fopen($dest, 'w');
+	stream_copy_to_stream($stream, $target);
+	fclose($target);
+
+	// Make sure the size declared by the browser is same as the one we received.
+	// This is mostly because on abort the request seems to be dumped into the
+	// script, if there's a difference of filesize there's a good chance it was
+	// an abort
+	if (filesize($dest) < $_SERVER['CONTENT_LENGTH'])
+	{
+		@unlink($dest);
+		exit;
+	}
+
+	// Do our basic attachment validation checks before counting this file in
+	if (!empty($settings['attachmentSizeLimit']) &&	filesize($dest) > $settings['attachmentSizeLimit'] * 1024)
+		multiattach_error('file_too_big', $dest);
+	if (!empty($settings['attachmentNumPerPostLimit']) && (count($_SESSION['temp_attachments']) + 1) > $settings['attachmentNumPerPostLimit'])
+		multiattach_error('attachments_limit_per_post', $dest);
+
+	$total_size = 0;
+	foreach ($_SESSION['temp_attachments'] as $attach => $dummy)
+		$total_size += filesize($current_attach_dir . '/' . $attach);
+	$total_size += filesize($dest);
+
+	if (!empty($settings['attachmentPostLimit']) && $total_size > $settings['attachmentPostLimit'] * 1024)
+		multiattach_error('file_too_big', $dest);
+
+	if (!empty($settings['attachmentDirSizeLimit']))
+	{
+		// Make sure the directory isn't full.
+		$dirSize = 0;
+		$dir = @scandir($current_attach_dir) or multiattach_error('cant_access_upload_path', $dest);
+		foreach ($dir as $file)
+		{
+			if ($file == '.' || $file == '..')
+				continue;
+
+			if (preg_match('~^post_tmp_\d+_\d+$~', $file) != 0)
+			{
+				// Temp file is more than 5 hours old!
+				if (filemtime($current_attach_dir . '/' . $file) < time() - 18000)
+					@unlink($current_attach_dir . '/' . $file);
+				continue;
+			}
+
+			$dirSize += filesize($current_attach_dir . '/' . $file);
+		}
+
+		// Too big! Maybe you could zip it or something...
+		if (filesize($dest) + $dirSize > $settings['attachmentDirSizeLimit'] * 1024)
+			multiattach_error('ran_out_of_space', $dest);
+	}
+
+	$_SESSION['temp_attachments'][$attachID] = $filename;
+
+	@chmod($dest, 0644);
+
+	echo json_encode(array('valid' => true, 'id' => $attachID, 'name' => $filename));
+	exit;
+}
+
+/**
+ * Returns the current attachment directory
+ *
+ * @return string
+ */
+function get_attach_dir()
+{
+	global $settings;
+
+	if (!empty($settings['currentAttachmentUploadDir']))
+	{
+		if (!is_array($settings['attachmentUploadDir']))
+			$settings['attachmentUploadDir'] = unserialize($settings['attachmentUploadDir']);
+
+		// Just use the current path for temp files.
+		$current_attach_dir = $settings['attachmentUploadDir'][$settings['currentAttachmentUploadDir']];
+	}
+	else
+		$current_attach_dir = $settings['attachmentUploadDir'];
+
+	return $current_attach_dir;
+}
+
+/**
+ * Throws an error on our behalf
+ *
+ * @param string $error_code
+ * @param string $filepath
+ * @return void
+ */
+function multiattach_error($error_code, $filepath = '')
+{
+	global $txt, $language;
+
+	if (!empty($filepath))
+		@unlink($filepath);
+
+	loadLanguage(array('Errors', 'Post'), $language);
+	echo json_encode(array('valid' => false, 'error' => isset($txt[$error_code]) ? $txt[$error_code] : $error_code));
+	exit;
+}
+
+?>
