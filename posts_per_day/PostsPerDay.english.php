853ca96bef5359e7884915a861159cab564479ee
Arantor <sleeping@myperch.org>
2012-12-05 03:53:47 +0000

    + [Posts Per Day] Initial checkin.

---
+++ posts_per_day/PostsPerDay.english.php
@@ -0,0 +1,7 @@
+<?php
+
+$txt['modfilter_condtype_postsperday'] = 'Depends on how many posts the user made in the last 24 hours';
+$txt['modfilter_cond_postsperday'] = 'Number of posts in the last 24 hours';
+$txt['modfilter_postsperday_is'] = 'The number of posts in the last 24 hours is';
+
+?>
---
+++ posts_per_day/PostsPerDay.php
@@ -0,0 +1,47 @@
+<?php
+
+function postsperday_moderation_rules(&$known_variables, $admin)
+{
+	global $txt, $admin_areas;
+
+	loadPluginLanguage('Arantor:PostsPerDay', 'PostsPerDay');
+	loadPluginTemplate('Arantor:PostsPerDay', 'PostsPerDay');
+	$known_variables['postsperday'] = array(
+		'type' => 'range',
+		'current' => 0,
+		'func_val' => 'count_postsperday',
+		'function' => create_function('$criteria', '
+			global $txt;
+			return $txt[\'modfilter_cond_\' . $criteria[\'name\']] . \': \' . $txt[\'modfilter_range_\' . $criteria[\'term\']] . \' \' . $criteria[\'value\'];
+		'),
+	);
+}
+
+function displayRow_postsperday($rule)
+{
+	return simpleRange_displayRow($rule, 'postsperday');
+}
+
+function count_postsperday($subject, $body)
+{
+	global $user_info;
+	if ($user_info['is_guest'])
+		return 0;
+
+	$request = wesql::query('
+		SELECT COUNT(id_msg)
+		FROM {db_prefix}messages
+		WHERE id_member = {int:member}
+			AND poster_time >= {int:time}',
+		array(
+			'member' => $user_info['id'],
+			'time' => time() - 86400,
+		)
+	);
+	list ($count) = wesql::fetch_row($request);
+	wesql::free_result($request);
+
+	return $count;
+}
+
+?>
