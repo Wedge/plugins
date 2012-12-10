78bba8ec43c20510f080b22d5a9bd8f5d6a875fd
Arantor <sleeping@myperch.org>
2012-12-10 00:17:02 +0000

    ! [WedgeDotOrg] Initial checkin. This particular plugin handles the SVN rev number in the footer. I'll be removing this bit from trunk in my next commit.

---
+++ wedgedotorg/WedgeDotOrg.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('WEDGE'))
+	die('Hacking attempt...');
+
+// Add the revision number to the footer. It's pretty quick and dirty.
+function wedgedotorg_theme()
+{
+	global $theme, $txt;
+
+	$rev = @file_get_contents($theme['default_theme_dir'] . '/rev.txt');
+	if (!empty($rev))
+		$txt['copyright'] .= ' (rev <a href="http://wedge.org/pub/feats/6108/new-revs/">' . $rev . '</a>)';
+}
+
+?>
