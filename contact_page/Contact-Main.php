4ba94d6cb4bdd7896f47849aa7c1b0e56149f953
live627 <live627@gmail.com>
2013-06-20 03:47:40 +0000

    ! [Contact] Fix menu button positioning and icon

--- contact_page/Contact-Main.php
+++ contact_page/Contact-Main.php
@@ -15,10 +15,10 @@ function contactMenu(&$menu_buttons)
 			'show' => allowedTo('view_contact_page'),
 		),
 	);
-	$menu_buttons = array_insert($menu_buttons, 'search', $item, 'after');
+	$menu_buttons = array_insert($menu_buttons, 'home', $item, true);
 
 	add_css('
-	.m_contact { float: left; width: 16px; height: 16px; padding: 0; background: url("' . $context['plugins_url']['Arantor:ContactPage'] . '/contact_small.png") no-repeat 0 0; margin:4px 4px 0 2px; }');
+	#m_contact { float: left; width: 16px; height: 16px; padding: 0; background: url("' . $context['plugins_url']['Arantor:ContactPage'] . '/contact_small.png") no-repeat 0 0; margin:4px 4px 0 2px; }');
 }
 
 function Contact()
