04f4daa38440ca32bd84d11334578ba13779bc7d
Sven Rissmann <creaworld-media@gmx.de>
2014-01-28 20:30:36 +0100

    This is a little handy plugin that brings a floating button for Pinterest into wedge. Initial version.
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ we_pinterest/We-Pinterest-Admin.english.php
@@ -0,0 +1,7 @@
+<?php
+
+$txt['we_pinterest'] = 'Pinterest';
+$txt['we_pinterest_desc'] = 'From this page you can enable a floating Pin It button for Pinterest to all images of your forum.';
+$txt['we_pinterest_not_available'] = 'We-Pinterest cannot be used when guests cannot access the forum!';
+$txt['we_pinterest_on'] = 'Enable the Pinterest Pin It button';
+?>
---
+++ we_pinterest/readme.german.txt
@@ -0,0 +1,6 @@
+Dies ist die Intergration von Pinterest für Wedge.
+
+Ein kleines Plugin um einen Hover "Pin It" Button für jedes Bild in Deinem Forum zu erhalten.
+Dieses Plugin wird noch um die komplette Pinterest API erweitert.
+
+Um dieses Plugin zu verwenden, muss dieses unter: Admin->Plugins->We-Pinterest aktiviert werden.
