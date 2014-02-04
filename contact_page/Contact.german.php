388ee6c9d4aae170b2c3e556a16984a2c0f7e979
Sven Rissmann <creaworld-media@gmx.de>
2014-02-04 18:42:14 +0100

    German translation for plugin contact page
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ contact_page/Contact.german.php
@@ -0,0 +1,27 @@
+<?php
+
+$txt['contact_us'] = 'Kontaktiere uns';
+
+$txt['send_message'] = 'Nachricht senden';
+$txt['contact_name'] = 'Dein Name';
+$txt['contact_subject'] = 'Betreff deiner Nachricht';
+$txt['contact_body'] = 'Deine Nachricht';
+$txt['contact_email'] = 'Deine Email-Adresse';
+
+$txt['error_no_contact_name'] = 'Es wurde kein Name ausgewählt.';
+$txt['error_no_contact_email'] = 'Es wurde keine gültige Email-Adresse ausgewählt.';
+$txt['error_no_contact_subject'] = 'Es wurde kein Betreff eingegeben.';
+$txt['error_no_contact_body'] = 'Es wurde keine Nachricht ausgewählt';
+
+$txt['contact_errors'] = 'Es gab Probleme mit deiner Nachricht:';
+
+$txt['cannot_view_contact_page'] = 'Dir ist es nicht gestattet, die Kontakt-Seite aufzurufen.';
+
+$txt['contact_message'] = 'Ein User hat Dir eine Nachricht über das Kontaktformular zukommen lassen.';
+$txt['user_name'] = 'Name des Users: %1$s';
+$txt['user_email'] = 'Emailadresse des Users: %1$s';
+$txt['user_subject'] = '[Contact Form] %1$s';
+$txt['user_body'] = 'Die Nachricht:' . "\n\n" . '%1$s';
+
+$txt['message_sent'] = 'Deine Nachricht wurde verschickt! Du kannst nun auf die <a href="<URL>">Hauptseite</a> zurückkehren.';
+?>
