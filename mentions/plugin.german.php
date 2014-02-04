a804007b512d83d64d1c83ff8cdd8c93af4e898d
Sven Rissmann <creaworld-media@gmx.de>
2014-02-04 19:20:44 +0100

    German translation for plugin mentions
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ mentions/plugin.german.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WeMentions' plugins language file
+ *
+ * @package Dragooon:WeMentions
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2013, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *      Licensed under "New BSD License (3-clause version)"
+ *      http://www.opensource.org/licenses/BSD-3-Clause
+ * @version 1.0
+ */
+
+$txt['notifier_mentions_title'] = 'Mitglieder Benachrichtigungen';
+$txt['notifier_mentions_desc'] = 'Erlaubt die Benachrichtigung, wenn ein Mitglied ein anderes Mitglied via @<Benutzername> erwähnt';
+$txt['notifier_mentions_subject'] = 'Du wurdest in einem Beitrag erwähnt!';
+$txt['notifier_mentions_html'] = '{MEMBER_LINK} erwähnt dich in "{OBJECT_LINK}".';
+$txt['notifier_mentions_text'] = '{MEMBER_NAME} hat dich im Beitrag "{OBJECT_NAME}" erwähnt. Du kannst den Beitrag hier nachlesen:
+
+{OBJECT_URL}';
+$txt['permissionname_mention_member'] = 'Erwähnte (@)Mitglieder';
+$txt['permissionhelp_mention_member'] = 'Erlaubt die Benachrichtigung, wenn ein Mitglied mit @Benutzername in einem Beitrag erwähnt wurde';
