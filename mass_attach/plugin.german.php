77e6fa2ce0f5b4ee0c0cc9be3f37e89488038d9c
Sven Rissmann <creaworld-media@gmx.de>
2014-02-04 19:11:12 +0100

    German translation for plugin mass_attach.
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ mass_attach/plugin.german.php
@@ -0,0 +1,6 @@
+<?php
+
+$txt['massattach_drag_help'] = 'Hier die Dateien einfügen';
+$txt['massattach_drag_help_subtext'] = 'Um sie an einen Beitrag anzuhängen';
+
+$txt['massattach_currently_uploading'] = 'Lade die Dateien hoch. Bitte warte, bis alle Dateien hochgeladen wurden, oder klicke auf Abbrechen.';
