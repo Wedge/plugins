414f0f9487bf37d1307b5aaf80212f640bb06f00
Sven Rissmann <creaworld-media@gmx.de>
2014-02-04 19:50:24 +0100

    German translation for plugin users_online_today.
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ users_online_today/OnlineToday-Admin.german.php
@@ -0,0 +1,21 @@
+<?php
+// Important! Before editing these language files please read the text at the top of index.english.php.
+// Don't forget to set the language file to UTF-8 format (without signature) before saving it.
+
+$txt['uot'] = 'Benutzer online heute';
+$txt['uot_type'] = 'Was sollen die User angezeigt bekommen?';
+$txt['uot_today'] = 'Benutzer die heute angemeldet waren (basierend auf Serverzeit)';
+$txt['uot_24h'] = 'Benutzer die in dne letzten 24 Stunden angemeldet waren';
+$txt['uot_7d'] = 'Benutzer, die in den letzten 7 Tagen angemeldet waren';
+
+$txt['uot_whoview'] = 'Wer darf die Liste mit den Online-Benutzern sehen?';
+$txt['uot_whoview_any'] = 'Alle';
+$txt['uot_whoview_members'] = 'Alle angemeldeten Benutzer';
+$txt['uot_whoview_staff'] = 'Administratoren ond Global-Moderatoren';
+$txt['uot_whoview_admin'] = 'Nur der Administrator';
+
+$txt['uot_order'] = 'Wie soll die Benutzeranzeige sortiert werden?';
+$txt['uot_order_name_asc'] = 'Nach Name, A-Z';
+$txt['uot_order_name_desc'] = 'Nach Name, Z-A';
+$txt['uot_order_time_asc'] = 'Nach der letzten Online-Zeit, aufsteigend';
+$txt['uot_order_time_desc'] = 'Nach der letzten Online-Zeit, absteigend';
---
+++ users_online_today/OnlineToday.german.php
@@ -0,0 +1,9 @@
+<?php
+
+$txt['users_online_today'] = 'Benutzer heute';
+$txt['users_online_24h'] = 'Benutzerin den letzten 24 Stunden';
+$txt['users_online_7d'] = 'Benutzer in den letzten 7 Tagen';
+$txt['users_online_today_userhidden'] = '%1$s%2$s'; // For i18n. It's not always possible to have all the combinations but between this and users/hidden combinations, it should cover everything.
+$txt['users_online_today_users'] = array(1 => '1 Benutzer', 'n' => '%s Benutzer');
+$txt['users_online_today_hidden'] = ' inklusive %s versteckte'; // doesn't matter how many are hidden, the same applies in English: 1 hidden, 10 hidden etc. You may add an array as needed.
+$txt['users_online_today_none'] = 'Heute waren noch keine Benutzer online.';
