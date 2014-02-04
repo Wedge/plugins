6e21be063a61e035db5b117b9adeb8921963283c
Sven Rissmann <creaworld-media@gmx.de>
2014-02-04 18:21:54 +0100

    Missing strings in the Birthday plugin.  (Birthday-Admin.english.php)
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ birthday/Birthday-Admin.german.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Wedge: Birthdays
+ *
+ * Language strings for the administration panel.
+ *
+ * @package wedge-birthdays
+ * @copyright 2010-2011 Wedgeward, wedge.org, 2011 Simple Machines, simplemachines.org
+ * @license http://wedge.org/license/
+ */
+
+$txt['birthdays'] = 'Geburtstage';
+$txt['birthday_info_center'] = 'Geburtstage im Info-Center anzeigen?';
+$txt['birthday_info_center_days'] = 'Für wie viele Tage?';
+$txt['scheduled_task_birthdays'] = 'Verarbeitung von Geburtstagen';
+$txt['scheduled_task_desc_birthdays'] = 'Tägliche Routine, die für die Geburtstage benötigt wird. Einschließlich der Benachrichtigungen.';
+
+$txt['birthday_show_ages'] = 'Zeige das Alter der Mitglieder an';
+
+$txt['birthday_send_email'] = 'Email an Mitglieder an ihrem Geburtstag verschicken?';
+$txt['birthday_email'] = 'Geburtstagsnachricht die übermittelt werden soll';
+$txt['birthday_body'] = 'Email Nachricht';
+$txt['birthday_subject'] = 'Email Betreff';
+
+$helptxt['birthday_email'] = 'Wähle einen Index aus, den das Geburtstags-Plugin verwenden soll. Eine Vorschau wird im Betreff und im Nachrichtenfeld sichtbar..<br><strong>Beachte:</strong> Damit diese Funktion ausgeführt werden kann, muss die geplante Aufgabe für Geburtstage aktiviert sein.  Diese Aufgabe sollte eigentlich direkt nach der Installation aktiviert sein. Du kannst die Funktion gerne durch einen Klick auf die <a href="%1$s?action=admin;area=scheduledtasks;%2$s" target="_blank" class="new_win">Geplanten Aufgaben</a> überprüfen.';
+
+?>
