8ec350c25b5b7c00f10221bf5e66f5096c6ff90f
Sven Rissmann <creaworld-media@gmx.de>
2014-02-04 18:54:14 +0100

    German translation for plugin edit_history
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ edit_history/EditHistory.german.php
@@ -0,0 +1,13 @@
+<?php
+
+$txt['view_history'] = array('n' => 'Ansicht von %1$s Änderungen', 1 => 'Betrachte 1 Änderung');
+
+$txt['edit_history'] = 'Edit Verlauf';
+$txt['edit_history_invalid'] = 'Der Edit-Verlauf konnte nicht geladen werden.';
+
+$txt['edited_by'] = 'Geändert von';
+$txt['edited_on'] = 'Änderungen';
+$txt['current'] = '(aktuell)';
+$txt['view_post'] = 'Ansehen';
+
+$txt['this_edit_by_on'] = 'Diese Änderung wurde von %1$s an %2$s durchgeführt';
