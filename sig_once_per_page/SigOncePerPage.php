e6c3ef939c23ce5c320f144a4ee8dd5b21c63d3f
Nao <nao@wedge>
2014-02-07 19:33:17 +0100

    * Static arrays should be declared as arrays in the first place. It's not useful, but it's a helpful guideline. (sig_once_per_page/SigOncePerPage.php)
    Signed-off-by: Nao <nao@wedge>

--- sig_once_per_page/SigOncePerPage.php
+++ sig_once_per_page/SigOncePerPage.php
@@ -2,11 +2,9 @@
 
 function sig_once_per_page(&$counter, &$output)
 {
-	static $users;
+	static $users = array();
 
 	if (isset($users[$output['member']['id']]))
 		$output['member']['signature'] = '';
 	$users[$output['member']['id']] = true;
 }
-
-?>
