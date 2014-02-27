a6b626c5ed266ff0783571c5642e79ed12bfb1da
Nao <nao@wedge>
2014-02-27 17:56:38 +0100

    + This may not be too sexy, but this is how it works. I'm adding the Wedge.org plugin's CSS integration into the main CSS file, as a sample of what the latest Wedge commit can now do. It saves an extra HTTP request, and embeds the Facebook icon directly within the main CSS. It only adds 150 bytes or so, so don't you start complaining! ;) (wedge.org/WedgeDotOrg.php, wedge.org/wedge.org.css)
    Signed-off-by: Nao <nao@wedge>

--- wedge.org/WedgeDotOrg.php
+++ wedge.org/WedgeDotOrg.php
@@ -8,10 +8,12 @@ function wedgedotorg_theme()
 {
 	global $context, $txt;
 
+	$context['main_css_files'][$context['plugins_dir']['Wedge:Wedge.org'] . '/wedge.org.css'] = false;
+
 	$rev = @file_get_contents(CORE_DIR . '/rev.txt');
 	if (!empty($rev))
 		$txt['copyright'] .= ', ' . (we::$user['language'] == 'french' ? 'révision' : 'revision') . ' <a href="http://wedge.org/pub/feats/6108/new-revs/">' . $rev . '</a>';
 
 	$context['custom_credits'] = (empty($context['custom_credits']) ? '' : $context['custom_credits']) . ' |
-				' . (we::$user['language'] == 'french' ? 'Aimez-nous sur' : 'Like us on') . ' <img src="' . $context['plugins_url']['Wedge:Wedge.org'] . '/fb.gif" style="width: 12px; height: 12px; vertical-align: -1px; border: 0"> <a href="http://www.facebook.com/wedgebook">Facebook</a>.';
+				' . (we::$user['language'] == 'french' ? 'Aimez-nous sur' : 'Like us on') . ' <span class="likeus"><a href="http://www.facebook.com/wedgebook">Facebook</a></span>.';
 }
