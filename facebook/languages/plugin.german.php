6e4476a5f4f7223cee07ef8bb4ac61d30a8dd23b
Sven Rissmann <creaworld-media@gmx.de>
2014-03-01 08:42:38 +0100

    German translation for facebook plugin.
    Signed-off-by: Sven Rissmann <creaworld-media@gmx.de>

---
+++ facebook/languages/plugin.german.php
@@ -0,0 +1,29 @@
+<?php
+
+$txt['facebook'] = 'Facebook';
+$txt['facebook_login'] = 'Mit Facebook anmelden';
+$txt['facebook_register_pass_subtext'] = 'Das Passwort agiert als zweite Authentifizierungs-Methode und wird nicht abgefragt, wenn man sich über Facebook anmeldet. Trotzdem wird das Passwort an einigen Stellen abgefragt (Änderung des Accounts, etc.).';
+$txt['facebook_create_password'] = 'Passwort erstellen';
+$txt['facebook_user'] = 'Facebook User';
+$txt['facebook_app_id'] = 'Facebook App ID';
+$txt['facebook_app_id_subtext'] = 'Gebe deine Facebook App ID ein, die du von der Facebook Developer Platform erhalten hast';
+$txt['facebook_app_secret'] = 'Facebook App Secret';
+$txt['facebook_app_secret_subtext'] = 'Gebe deine App-Secret ein. Aus Sicherheitsgründen solltest du diesen Schlüssel nirgendwo veröffentlichen';
+$txt['facebook_admin_desc'] = 'Konfiguriere die Einstellungen für die Anmeldung über Facebook. Um eine APP ID und ein Secret anzugeben musst du eine Web-App von der <a href="https://developers.facebook.com/apps/" target="_blank">Facebook-Developer Platform</a> besitzen.';
+$txt['facebook_new_topic'] = 'Hat ein neues Thema in "%s" am %s erstellt!';
+$txt['facebook_profile_desc'] = 'Verändere deine Facebookeinstellunegn hier, verbinde deinen Account mit Facebook und lege fest was synchronisiert werden soll';
+$txt['facebook_profile_login_desc'] = 'Verknüpfe deinen Account mit Facebook und genieße das automatische Login. Automatisches veröffentlichen von Beiträgen, Gedanken und Statusmeldungen auf deiner Wall!';
+$txt['facebook_status_ok'] = 'Facebook-Status ist O.K!';
+$txt['facebook_logged_in'] = 'Du bist mit Facebook von dieser Seite aus verbunden';
+$txt['facebook_id'] = 'Facebook ID #%s';
+$txt['facebook_sync_name'] = 'Sync-Name';
+$txt['facebook_sync_name_subtext'] = 'Jede Namensänderung auf Facebook wird automatisch auch auf dieser Seite angewendet';
+$txt['facebook_sync_birthday'] = 'Geburtstage synchronisieren';
+$txt['facebook_sync_birthday_subtext'] = 'Jede Änderung des Geburtstags wird automatisch auch auf dieser Seite angewendet';
+$txt['facebook_sync_feed'] = 'Status aktualisierungen zu Gedanken';
+$txt['facebook_sync_feed_subtext'] = 'Wenn du eine Statusänderung auf Facebook durchführst, wird hier automatisch ein Gedanke veröffentlicht.';
+$txt['facebook_sync_thoughttofeed'] = 'Neue Gedanken veröffentlichen';
+$txt['facebook_sync_thoughttofeed_subtext'] = 'Wenn du einen neuen Gedanken erstellst, wird dieser automatisch auch auf deiner Wall veröffentlicht.';
+$txt['facebook_sync_topictofeed'] = 'Neue Themen veröffentlichen';
+$txt['facebook_sync_topictofeed_subtext'] = 'Wenn du eine neues Thema eröffnest, wird dieses automatisch auf deiner Wall veröffentlicht.';
+$txt['facebook_user_already_exists'] = 'Sorry, dieser Facebook-Account ist bereit mit dem Benutzer <a href="<URL>?action=profile;u=%1$d">%2$s</a> verknüpft.';
