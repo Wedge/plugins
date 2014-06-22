551c4029abe3a0a49528e55ec7eaaede546ff35e
Nao <nao@wedge>
2014-06-22 20:58:09 +0200

    ! b.board_order needs to be specified for InnoDB. (ManageCalendar.php)
    Signed-off-by: Nao <nao@wedge>

--- calendar/ManageCalendar.php
+++ calendar/ManageCalendar.php
@@ -338,9 +338,8 @@ function ModifyCalendarSettings($return_config = false)
 	$request = wesql::query('
 		SELECT b.id_board, b.name AS board_name, c.name AS cat_name
 		FROM {db_prefix}boards AS b
-			LEFT JOIN {db_prefix}categories AS c ON (c.id_cat = b.id_cat)',
-		array(
-		)
+			LEFT JOIN {db_prefix}categories AS c ON (c.id_cat = b.id_cat)
+		ORDER BY b.board_order'
 	);
 	while ($row = wesql::fetch_assoc($request))
 		$boards[$row['id_board']] = $row['cat_name'] . ' - ' . $row['board_name'];
