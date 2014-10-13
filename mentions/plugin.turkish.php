256ea0bd9e3cff78f9fe9ce465084a4d007b99e3
Ramazan Subaşi <johndoe@example.com>
2014-10-13 21:07:54 +0300

    Turkish Language File added

---
+++ mentions/plugin.turkish.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * WeMentions' plugins language file
+ *
+ * @package Dragooon:WeMentions
+ * @author Shitiz "Dragooon" Garg <Email mail@dragooon.net> <Url http://smf-media.com>
+ * @copyright 2013, Shitiz "Dragooon" Garg <mail@dragooon.net>
+ * @license
+ *      Licensed under "New BSD License (3-clause version)"
+ *      http://www.opensource.org/licenses/BSD-3-Clause
+ * @version 1.0
+ */
+
+$txt['notifier_mentions_title'] = 'Uye Etiketleme';
+$txt['notifier_mentions_desc'] = 'Uyelerin diger uyeleri @<uye adı> seklinde etiketlemesine ve bildirimlere izin ver';
+$txt['notifier_mentions_subject'] = 'Bir yorumda senden bahsedildi!';
+$txt['notifier_mentions_html'] = '{MEMBER_LINK} Bu yorunmda etiketlendin "{OBJECT_LINK}".';
+$txt['notifier_mentions_text'] = '{MEMBER_NAME} seni su yorumda etiketledi: "{OBJECT_NAME}". Yorumu buradan okuyabilirsin:
+
+{OBJECT_URL}';
+$txt['permissionname_mention_member'] = 'Uye Etiketleme';
+$txt['permissionhelp_mention_member'] = 'Uyeleri @ isaret ile etiketlemeye ve bildirimlere izin verir';
+
+
+
