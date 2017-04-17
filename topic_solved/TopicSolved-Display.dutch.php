0a325863cfdbb33970034ff15d340b55dce2d5d9
MyMug <7french@gmail.com>
2017-04-17 09:12:27 +0200

    TopicSolved-Display.dutch.php
    Dutch translation

---
+++ topic_solved/TopicSolved-Display.dutch.php
@@ -0,0 +1,7 @@
+<?php
+
+$txt['topic_was_solved_missing_author'] = 'Dit topic is gemarkeerd als opgelost, %1$s';
+$txt['topic_was_solved_author'] = 'Dit topic is gemarkeerd als opgelost door topic starter, %1$s';
+$txt['topic_was_solved_non_author'] = 'Dit topic is gemarkeerd als opgelost door %1$s, %2$s';
+$txt['topic_mark_solved'] = 'Markeer Topic als Opgelost.';
+$txt['topic_mark_unsolved'] = 'Markeer Topic als (nog) niet Opgelost.';
