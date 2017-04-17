466c671663d47da143d17661cdb604a53c47f5c2
MyMug <7french@gmail.com>
2017-04-17 09:09:23 +0200

    TopicSolved-Admin.dutch.php
    Dutch translation

---
+++ topic_solved/lang/TopicSolved-Admin.dutch.php
@@ -0,0 +1,21 @@
+<?php
+
+$txt['topicsolved'] = 'Topic opgelost';
+$txt['topicsolved_style'] = 'Opgeloste onderwerpen worden weergegeven met een andere kleur in de lijst met onderwerpen. U kunt dit hier instellen of kiezen, onderwerpen hebben twee verschillende achtergrondkleuren, en beide zijn hier geconfigureerd.';
+$txt['topicsolved_bg1'] = 'De donkere kolom achtergrond kleur';
+$txt['topicsolved_bg2'] = 'De lichtere kolom achtergrond kleur';
+$txt['topicsolved_fg'] = 'Tekst kleuren';
+$txt['topicsolved_boards'] = 'Boards waarin Topics als opgelost gemarkeerd kunnen worden';
+
+$txt['modlog_ac_solve'] = 'Markeer &quot;{topic}&quot; als opgelost';
+$txt['modlog_ac_unsolve'] = 'Markeer &quot;{topic}&quot; als niet opgelost';
+
+$txt['topicsolved_own'] = 'Wie kan zijn eigen topic als opgelost markeren';
+$txt['topicsolved_any'] = 'Wie kan ieder Topic als opgelost markeren';
+$txt['permissionname_topicsolved'] = 'Markeer een Topic als opgelost';
+$txt['permissionname_topicsolved_own'] = 'Eigen topics';
+$txt['permissionname_topicsolved_any'] = 'Ieder Topic';
+$txt['permissiongroup_ts_group'] = 'Topic opgelost';
+
+$txt['group_perms_name_topicsolved_own'] = 'Markeer eigen Topic als opgelost';
+$txt['group_perms_name_topicsolved_any'] = 'Markeer ieder Topic als opgelost';
